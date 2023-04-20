556261ce0726ad893f52a56470852c7e28d8eac9
Miranda Cascione <miranda.cascione@gmail.com>
2023-04-19 21:58:40 -0700

    show.test.js mvp

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -3,8 +3,17 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Display from './../Display';
 
-test('renders without errors with no props', async () => { });
 
-test('renders Show component when the button is clicked ', () => { });
+test('renders without errors with no props', async () => {
+    render (<Display />);
+});
 
-test('renders show season options matching your data when the button is clicked', () => { });
+test('renders Show component when the button is clicked ', async() => {
+    render (<Display />);
+    fireEvent.click(screen.getByRole('button'));
+    await expect(screen.findByRole('header')).toBeInTheDocument;
+ });
+
+test('renders show season options matching your data when the button is clicked', () => {
+
+ });
